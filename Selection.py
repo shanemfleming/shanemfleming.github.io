e5161f56cd176328082ecb0c7c4f8d851fec11e2
shanemfleming <72095388+shanemfleming@users.noreply.github.com>
2020-10-01 11:58:56 -0400

    Delete Selection.py

--- Selection.py
+++
@@ -1,9 +0,0 @@
-# Week 3 practice from Online Weekly Work
-
-year = int(input("Please enter the current year: "))
-if year % 4 == 0 and year % 100 != 0:
-    print(year, " is a leap year.")
-elif year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
-    print(year, " is a leap year.")
-else:
-    print(year, " is not a leap year.")
